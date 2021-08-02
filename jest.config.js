83cc56c334173cca5f36374e6e51fdf14135d051
LuannMateus <luanmateuslima34@gmail.com>
2021-08-02 18:49:53 -0300

    Start the project - settings and login logic

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+
+  collectCoverage: true,
+
+  coverageDirectory: 'coverage',
+
+  coverageProvider: 'v8'
+
+}
---
+++ src/login.js
@@ -0,0 +1,58 @@
+
+// Signup-router
+const express = require('express')
+const router = express.Router()
+
+module.exports = () => {
+  const router = new SignUpRouter()
+  router.post('/signup', ExpressRouterAdapter.adapt(router))
+}
+
+class ExpressRouterAdapter {
+  static adapt (router) {
+    return async (req, res) => {
+      const httpRequest = {
+        body: req.body
+      }
+
+      const httpResponse = await router.route(httpRequest)
+
+      res.status(httpResponse.statusCode).json(httpResponse.body)
+    }
+  }
+}
+
+// Presentation
+class SignUpRouter {
+  async route (httpRequest) {
+    const { email, password, repeatPassword } = httpRequest.body
+    const user = new SignUpUseCase().signUp(email, password, repeatPassword)
+
+    return {
+      statusCode: 200,
+      body: user
+    }
+  }
+}
+
+// Domain
+// SignUp use case
+class SignUpUseCase {
+  async signUp (email, password, repeatPassword) {
+    if (password === repeatPassword) {
+      new AddAccountRepository().add(email, password)
+    }
+  }
+}
+
+// Infra
+// Create user
+const mongoose = require('mongoose')
+const AccountModel = mongoose.model('Account')
+
+class AddAccountRepository {
+  async add (email, password) {
+    const user = await AccountModel.create({ email, password })
+    return user
+  }
+}
