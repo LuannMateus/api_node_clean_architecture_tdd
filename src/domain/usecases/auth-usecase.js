b3b60db882ceb2562c1d0a935d1599a61473a1ba
LuannMateus <luanmateuslima34@gmail.com>
2021-08-05 20:40:44 -0300

    Simplify code

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -18,19 +18,14 @@ class AuthUseCase {
 
     const user = await this.loadUserByEmailRepository.load(email)
 
-    if (!user) {
-      return null
+    const isValid = user && await this.encrypter.compare(password, user.password)
+
+    if (isValid) {
+      const accessToken = await this.tokenGenerator.generate(user.id)
+      return accessToken
     }
 
-    const isValid = await this.encrypter.compare(password, user.password)
-
-    if (!isValid) {
-      return null
-    }
-
-    const accessToken = await this.tokenGenerator.generate(user.id)
-
-    return accessToken
+    return null
   }
 }
 
