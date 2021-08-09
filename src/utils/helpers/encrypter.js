2c8cbafda47d3aa07c4b3c24e948c7e916d5c9c1
LuannMateus <luanmateuslima34@gmail.com>
2021-08-09 17:06:21 -0300

    Move Encrypter to its own file

---
+++ src/utils/helpers/encrypter.js
@@ -0,0 +1,11 @@
+const bcrypt = require('bcrypt')
+
+class Encrypter {
+  async compare (value, hash) {
+    const isValid = await bcrypt.compare(value, hash)
+
+    return isValid
+  }
+}
+
+module.exports = Encrypter
